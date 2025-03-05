16196c19273c07988e86c5204efa7bc462a984f0
Hubbell_Telescope <zachary4h@gmail.com>
2025-03-04 22:18:41 -0500

    Finished through 7

--- data/lahman.sql
+++ data/lahman.sql
@@ -1,10 +1,10 @@
 -- 1. Find all players in the database who played at Vanderbilt University. Create a list showing each player's first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 SELECT namefirst || ' ' || namelast, SUM(salary) AS sum_sal
-FROM collegeplaying
-INNER JOIN people
+FROM collegeplaying AS c
+INNER JOIN people AS p
 USING(playerid)
-INNER JOIN salaries
-USING(playerid)
+INNER JOIN salaries AS s
+ON p.playerid = s.playerid AND s.yearid = c.yearid
 WHERE schoolid = 'vandy'
 GROUP BY namefirst, namelast
 ORDER BY sum_sal DESC;
@@ -20,9 +20,7 @@ SELECT CASE
 			WHEN pos = 'C' THEN 'Battery'
 			ELSE 'N/A'
 		END AS position, COUNT(*) AS outputs
-FROM people AS p
-INNER JOIN fielding AS f
-USING(playerid)
+FROM fielding
 WHERE yearid = 2016
 GROUP BY position;
 
@@ -46,14 +44,94 @@ ON t.yearid < (decades + 10) AND t.yearid >= decades
 GROUP BY decades;
 
 -- 4. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases. Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
+SELECT p.namefirst, p.namelast, SUM(b.sb) AS stolen_bases, SUM(b.cs) AS caught_stealing, SUM(b.sb+b.cs) AS stolen_attempts, (100.0*SUM(b.sb)/SUM(b.sb+b.cs)) AS percentage_stolen
+FROM people AS p
+INNER JOIN batting AS b
+USING(playerid)
+WHERE b.yearid = 2016
+GROUP BY namefirst, namelast
+HAVING SUM(b.sb+b.cs) >= 20
+ORDER BY percentage_stolen DESC;
 
 -- 5. From 1970 to 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion; determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 to 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+SELECT teamid, yearid, MAX(W) AS max_winnings
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016 AND WSWin = 'N'
+GROUP BY teamid, yearid
+ORDER BY max_winnings DESC; 
+
+SELECT teamid, yearid, MAX(W) AS max_winnings
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016 AND WSWin = 'Y' AND yearid <> 1981
+GROUP BY teamid, yearid
+ORDER BY max_winnings ASC;
+
+SELECT teamid, yearid, MAX(W) AS max_winnings, WSWin
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016
+GROUP BY teamid, yearid, WSWin
+ORDER BY max_winnings DESC;
 
 -- 6. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+WITH nat_league AS (
+	SELECT DISTINCT playerid, lgid
+	FROM awardsmanagers
+	WHERE awardid = 'TSN Manager of the Year' AND lgid = 'NL'
+)
+SELECT playerid
+FROM awardsmanagers a
+INNER JOIN nat_league n
+USING(playerid)
+WHERE a.awardid = 'TSN Manager of the Year' AND a.lgid = 'AL';
+
+
+WITH award_managers AS (
+	SELECT playerid, a.yearid AS AL_year, NL_year
+	FROM awardsmanagers a
+	INNER JOIN (
+		SELECT DISTINCT playerid, lgid, yearid AS NL_year
+		FROM awardsmanagers
+		WHERE awardid = 'TSN Manager of the Year' AND lgid = 'NL'
+		) AS n
+	USING(playerid)
+	WHERE a.awardid = 'TSN Manager of the Year' AND a.lgid = 'AL'
+	)
+SELECT namegiven, AL_year, NL_year, teamid
+FROM people p
+INNER JOIN award_managers a
+USING(playerid)
+INNER JOIN managers m
+ON m.playerid = p.playerid AND (a.AL_year = m.yearid OR a.NL_year = m.yearid)
+
+
 
 -- 7. Which pitcher was the least efficient in 2016 in terms of salary / strikeouts? Only consider pitchers who started at least 10 games (across all teams). Note that pitchers often play for more than one team in a season, so be sure that you are counting all stats for each player.
+SELECT namegiven, SUM(s.salary) AS sal, SUM(pi.so) AS strikeouts
+FROM pitching AS pi
+INNER JOIN people AS p
+ON pi.playerid = p.playerid
+INNER JOIN salaries AS s
+ON pi.playerid = s.playerid AND pi.yearid = s.yearid
+WHERE s.yearid = 2016
+GROUP BY namegiven
+ORDER BY namegiven, sal DESC, strikeouts ASC;
+
+
 
 -- 8. Find all players who have had at least 3000 career hits. Report those players' names, total number of hits, and the year they were inducted into the hall of fame (If they were not inducted into the hall of fame, put a null in that column.) Note that a player being inducted into the hall of fame is indicated by a 'Y' in the **inducted** column of the halloffame table.
+WITH hall_of_famers AS (
+	SELECT playerid, yearid
+	FROM halloffame
+	WHERE inducted = 'Y'
+)
+SELECT playerid, namegiven, SUM(H) AS total_hits, h.yearid year_inducted
+FROM people p
+INNER JOIN batting b
+USING(playerid)
+LEFT JOIN hall_of_famers h
+USING(playerid)
+GROUP BY playerid, namegiven, h.yearid
+HAVING SUM(H) >= 3000;
 
 -- 9. Find all players who had at least 1,000 hits for two different teams. Report those players' full names.
 
